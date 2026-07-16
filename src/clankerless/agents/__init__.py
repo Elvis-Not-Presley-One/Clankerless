5540622058cde841ea45c5de47d11c4c525c3b7a
Elvis-Not-Presley-One <lawnguy1201@gmail.com>
2026-07-16 15:55:58 -0400

    refactor(): switched to multiprocessing in arena.py using 8 cores to speed up large amount of games being played by * 8
    refactor(): added more built in agents
    
    refactor():  hypertuned value function player called FW along side F, this model is better than AB and also preforms a lot better than F

--- src/clankerless/agents/__init__.py
+++ src/clankerless/agents/__init__.py
@@ -1,11 +1,22 @@
 from catanatron.models.player import SimplePlayer, RandomPlayer
 from catanatron.players.mcts import MCTSPlayer
 from catanatron.players.minimax import AlphaBetaPlayer
+from catanatron.players.search import VictoryPointPlayer
+from catanatron.players.value import ValueFunctionPlayer, DEFAULT_WEIGHTS
+from catanatron.players.weighted_random import WeightedRandomPlayer
+
+MY_WEIGHTS = {**DEFAULT_WEIGHTS, 'longest_road' : 6, 'hand_synergy' : 35.0}
 
 AGENTS = {
     'random' : RandomPlayer,
     'simple' : SimplePlayer,
     'AB' : AlphaBetaPlayer,
-    'MCT' : MCTSPlayer
+    'MCT' : MCTSPlayer,
+    'weighted' : WeightedRandomPlayer,
+    'F' : ValueFunctionPlayer,
+    # FW stands for ValueFunctionalPlayer Custom Weighted
+    # i.e. I spent time to get the best weights possible, this so to find a model to run against
+    'FW' : lambda color : ValueFunctionPlayer(color, value_fn_builder_name='C', params=MY_WEIGHTS),
+    'VP' : VictoryPointPlayer
 }
 
